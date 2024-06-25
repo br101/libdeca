60e23973aad7ffd5f0497ff4aa9b00a9b4a92979
Bruno Randolf <br@einfach.org>
2024-06-25 17:40:50 +0100

    Add LADDR_PAR for ESP-IDF

--- dwutil.h
+++ dwutil.h
@@ -2,8 +2,10 @@
 #define DECA_UTIL_H
 
 #define ADDR_FMT  "%04X"
+
 #if ESP_PLATFORM
 #define LADDR_FMT "[%016llX]"
+#define LADDR_PAR(x) x
 #else
 // 64 bit printf is not available in NRF SDK
 #define LADDR_FMT "[%08lX%08lX]"
