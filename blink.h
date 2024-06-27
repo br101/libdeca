5e4e7c63a4fa74e994e4aad39e480b7055d38cc3
Bruno Randolf <br@einfach.org>
2024-06-27 08:06:30 +0100

    BLINK: Add not about short blink

--- blink.h
+++ blink.h
@@ -9,6 +9,7 @@
 typedef void (*blink_cb_t)(uint64_t src, uint32_t seq, uint64_t rx_ts,
 						   uint64_t time_ms, uint8_t battery);
 
+/* Note: short blink can only be received when frame filtering is disabled */
 bool blink_send_short(uint16_t src);
 void blink_handle_msg_short(const struct rxbuf* rx);
 bool blink_send_long(uint64_t src);
